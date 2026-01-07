8fd55f795d459c75724b38da23c7e4f59b138d5c
Ajay Jamale <ajayjamale90@gmail.com>
2026-01-07 21:13:34 +0530

    Removing additional logs from main spring application file

--- src/main/java/com/ajay/EcommerceMultiVendorApplication.java
+++ src/main/java/com/ajay/EcommerceMultiVendorApplication.java
@@ -8,8 +8,5 @@ public class EcommerceMultiVendorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceMultiVendorApplication.class, args);
-			System.err.println("------------------------------------");
-			System.err.println("Server started successfully....!");
-			System.err.println("------------------------------------");
 	}
 }	
